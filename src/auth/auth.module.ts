a3ed4a26e3488d8da0e0ed0cbce8814922ec74db
FaridFadilah <faridfadillah29@gmail.com>
2022-12-15 00:50:24 +0700

    updated last night

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [AuthService],
   providers: [AuthService]
 })
 export class AuthModule {}
